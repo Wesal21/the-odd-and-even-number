9fdd827a1f62f2aa107e442505fe13a49c4b2e0e
Eng.Wesal <eng-wesal@eng.wesal-HP-250-G7-Notebook-PC>
2021-04-22 00:22:43 +0300

    the numer is odd or even

---
+++ odd-even.js
@@ -0,0 +1,47 @@
+
+  {
+    const num = Number(prompt('Enter a number')) || 0;
+     
+  
+    
+    
+
+   
+    if( Number(num)%2===0)
+      {
+          alert (  Number(num)+"  "+ "is even");
+      }
+      else 
+      {
+          
+        alert (  Number(num)+"  " + "is odd");
+      }
+     
+    
+}
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+         
+    
+ 
+   
+ 
+ 
+    
